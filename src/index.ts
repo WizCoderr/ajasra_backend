ffdc04fa893bdba4df6893cd45dcaf62c5cd630f
wizcoderr <ssayam200@gmail.com>
2025-06-08 03:26:54 +0530

    bug Fix

--- src/index.ts
+++ src/index.ts
@@ -16,10 +16,10 @@ const PORT = process.env.PORT || 9000;
 
 // Middleware
 app.use(helmet());
-app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-    credentials: true
-}));
+// app.use(cors({
+//     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+//     credentials: true
+// }));
 
 app.use(express.json());
 app.use(express.static('public'));
