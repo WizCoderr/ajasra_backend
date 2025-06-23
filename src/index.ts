f088473fe9b98701709d79955c5be72ebe2da6ae
wizcoderr <ssayam200@gmail.com>
2025-06-23 23:42:06 +0530

    fix: Enable CORS preflight requests for all routes

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-app.options('*', cors());
 
 // Routes
 import authRoutes from './routes/auth.route';
